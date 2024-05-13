d59c53d2d92f16519e960e1fe3a3ce4d3e07b173
buffedcream <101608916+buffedcream@users.noreply.github.com>
2024-05-13 08:49:27 +0700

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/<repo>/",
 
   lang: "en-US",
   title: "Ayanime",
