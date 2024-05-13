cf50cd53d0dec20882834cc31b350ee4654c6bb9
buffedcream <101608916+buffedcream@users.noreply.github.com>
2024-05-13 10:57:08 +0700

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -21,6 +21,6 @@ export default sidebar({
       text: "Slides",
       icon: "person-chalkboard",
       link: "https://plugin-md-enhance.vuejs.press/guide/content/revealjs/demo.html",
-    },
+    }
   ],
 });
