1da59776099a8057935f618d6b76dc2b4c9007d5
buffedcream <101608916+buffedcream@users.noreply.github.com>
2024-05-13 07:56:50 +0700

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,7 +1,7 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
-import { MR_HOPE_AVATAR } from "./logo.js";
+import { POLAR_STAR } from "./logo.js";
 
 export default hopeTheme({
   custom: true,
