37fe98dc594598c32894b175c6c3e69c6a2898af
buffedcream <101608916+buffedcream@users.noreply.github.com>
2024-05-21 07:48:44 +0700

    add auth0 script

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,6 +3,12 @@ import theme from "./theme.js";
 import { getDirname, path} from "vuepress/utils"
 
 export default defineUserConfig({
+  // src/.vuepress/config.js
+...
+head : [
+  ....,
+  ["script", {src : "https://cdn.auth0.com/js/auth0-spa-js/1.13/auth0-spa-js.production.js"}]
+],
   base: "/",
 
   lang: "en-US",
