a19e4ab6bdf85fe35f182e0947ba6bebe4447d27
polaris <rendy.yp@petalmail.com>
2024-06-23 18:03:27 +0700

    setting cron job

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,20 @@
+import { exec } from 'child_process';
+import path from 'path';
+
 export default function handler(req, res) {
-  res.status(200).end('Hello Cron!');
+  // Jalankan script Node.js menggunakan child_process
+  exec(`node ${path.resolve(__dirname, '/src/GenerateMarkdown.js')}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      res.status(500).json({ error: error.message });
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      res.status(500).json({ error: stderr });
+      return;
+    }
+    console.log(`Stdout: ${stdout}`);
+    res.status(200).json({ message: 'Cron job executed successfully', output: stdout });
+  });
 }
