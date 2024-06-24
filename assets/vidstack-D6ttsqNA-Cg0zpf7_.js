52a63be0c671cf815684794b611b2dc9c96014e8
snap-star <101608916+snap-star@users.noreply.github.com>
2024-06-24 04:07:09 +0000

    Deploying to gh-pages from @ snap-star/polaris@354efa77987cec62bde59a6853e9e00413161820 🚀

---
+++ assets/vidstack-D6ttsqNA-Cg0zpf7_.js
@@ -0,0 +1,1 @@
+import{bd as i,be as t}from"./app-ClVCIYzK.js";class e{constructor(s){this.La=s}Xa(){i(this.ya)&&this.fg()}$(){t(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{e as R};
