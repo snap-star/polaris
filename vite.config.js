4859b7bb1acb0d1d4e91917f88bc3daebd543dda
polaris <rendy.yp@petalmail.com>
2024-07-15 01:02:19 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-// vite.config.js
-import Components from "unplugin-vue-components/vite";
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Pisahkan node_modules menjadi chunk tersendiri
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 50000, //ubah sesuai kebutuhan
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@popperjs/core': require.resolve('@popperjs/core/lib/index.js')
+    }
   },
   plugins: [
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
-    //....
     Components({
-      resolvers: [
-        (componentName) => {
-          if (componentName.startsWith("Va"))
-            return { name: componentName, from: "vuestic-ui" };
-        },
-        [ElementPlusResolver()],
-      ],
+      resolvers: [ElementPlusResolver()],
     }),
+    commonjs()
   ],
-};
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Pisahkan node_modules menjadi chunk tersendiri
+          vendor: ["element-plus", "@popperjs/core"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 50000, // ubah sesuai kebutuhan
+  },
+});
